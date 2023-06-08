7eed2a49630605cd9b28eaf61465beb04bdb9e9b
Gabriel Barros <gabriellima.a.barros@gmail.com>
2023-06-08 18:03:23 -0300

    code for azure resources provisioning

--- adb_unitycatalog_abi/main.tf
+++ adb_unitycatalog_abi/main.tf
@@ -9,3 +9,44 @@ terraform {
         }
     }
 }
+
+# Provisioning of Azure resources
+provider "azurerm" {
+    features {}
+}
+
+## Creating the default managed container
+
+### Referencing the existing storage account resources
+data "azurerm_storage_account" "storage_accounts" {
+    name_starts_with = "brewdatgb"
+    resource_group_name = var.rg_name
+}
+
+### Creating the container inside the storage account
+resource "azurerm_storage_container" "container" {
+    name = var.container_name
+    storage_account_name = var.silvergold_storage_account
+    container_access_type = "private"
+}
+
+## Creating Access Connector for Azure Databricks and giving it the necessary permissions
+
+### Creating the access connector
+resource "azurerm_databricks_access_connector" "access_connector" {
+    name = var.access_connector_name
+    resource_group_name = var.rg_name
+    location = var.location
+
+    identity {
+        type = "SystemAssigned"
+    }
+}
+
+### Assigning permissions to storage accounts
+resource "azurerm_role_assignment" "mi_data_contributr" {
+    for_each = data.azurerm_storage_account.storage_accounts
+    scope = each.value.id
+    role_definition_name = "Storage Blob Data Contributor"
+    principal_id = azurerm_databricks_access_connector.access_connector.identity[0].principal_id
+}
